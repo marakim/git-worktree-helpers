e7c19ca497ed2dbb3c30a86f2ef1acbed48d9ab1
Mara Kim <marakim@microsoft.com>
2022-02-19 03:23:46 -0800

    Check for clone errors

--- src/git-clone-for-worktree.ps1
+++ src/git-clone-for-worktree.ps1
@@ -18,41 +18,34 @@ If (${Directory} -eq [string]::Empty) {
 }
 
 
-If (!(Test-Path -Path "${Directory}")) {
-    New-Item -ItemType Directory -Path "${Directory}"
-}
-Elseif (Test-Path -Path "${Directory}") {
-    If (!((Get-ChildItem -Path "${Directory}" | Select-Object -First 1 | Measure-Object).Count -eq 0)) {
-        Write-Error -Message "fatal: destination path '${Directory}' already exists and is not an empty directory."
-        Exit 1
-    }
+${DirectoryExists} = Test-Path -Path "${Directory}"
+
+If ($SingleBranch) {
+    git clone --single-branch --bare "${Repository}" "${Directory}/.git"
 }
 Else {
-    Write-Error -Message "fatal: destination path '${Directory}' is not a directory."
+    git clone --bare "${Repository}" "${Directory}/.git"
+}
+
+If (-not $?) {
+    If (!${DirectoryExists}) {
+        Remove-Item ${Directory}
+    }
     Exit 1
 }
 
 
 Push-Location -Path "${Directory}"
 
-Try {
-    If ($SingleBranch) {
-        git clone --single-branch --bare "${Repository}" .git
-    }
-    Else {
-        git clone --bare "${Repository}" .git
-    }
+${DefaultBranch} = git rev-parse --abbrev-ref HEAD
 
-    ${DefaultBranch} = git rev-parse --abbrev-ref HEAD
+If ($SingleBranch) {
+    git config --add remote.origin.fetch "+refs/heads/${DefaultBranch}:refs/remotes/origin/${DefaultBranch}"
+}
+Else {
+    git config --add remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
+}
 
-    If ($SingleBranch) {
-        git config --add remote.origin.fetch "+refs/heads/${DefaultBranch}:refs/remotes/origin/${DefaultBranch}"
-    }
-    Else {
-        git config --add remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
-    }
-    git worktree add "${DefaultBranch}"
-}
-Finally {
-    Pop-Location
-}
+git worktree add "${DefaultBranch}"
+
+Pop-Location
